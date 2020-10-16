2c7623c377b4cb85079154f0f4366dc4c3cb0e0a
shareed <shanreed6@gmail.com>
2020-10-15 20:59:55 -0700

    context added, and cleaned up

--- think-piece/src/App.js
+++ think-piece/src/App.js
@@ -8,68 +8,6 @@ import Authentication from './components/Authentication';
 
 
 class App extends Component {
-  // componentDidMount = async () => {
-  //   //await for the firestore collection
-  //   const snapshot = await firestore.collection('post').get()
-  //   //go through the snapshot of the database and map them into objects
-  //           const posts = snapshot.docs.map(collectData)//function is extracted into the utils.js file
-  //           //Set the state
-  //           this.setState({posts})
-  //                 }
-
-                  // componentDidMount = async () => {
-                  //   //await for the firestore collection
-                  //   const snapshot = await firestore.collection('post').get()
-                  //   //go through the snapshot of the database and map them into objects
-                  //           const posts = snapshot.docs.map(doc => { return {id: doc.id, ...doc.data()}})
-                  //           //Set the state
-                  //           this.setState({posts})
-                  //                 }
-                
-
-
-
-  // componentDidMount = async () => {
-  //   //await will wait for the promise to reslove
-  //   const snapshot = await firestore.collection('post').get()
-  //                 snapshot.forEach(doc => {
-  //                   const id = doc.id;
-  //                   const data = doc.data();
-  //                   console.log({ id, data});
-                    
-  //                 })
-  //                   console.log({snapshot});//returns a query snapshot
-  //                 }
-
-//With aysnc and await
-  // componentDidMount = async () => {
-  //   const snapshot = await firestore//await will wait for the promise to reslove
-  //                 .collection('posts')
-  //                 .get()
-  //                   console.log({snapshot});//returns a query snapshot
-  //                 }
-
-
-//Without aysnc and await
-  // componentDidMount = () => {
-  //   const posts = firestore
-  //                 .collection('posts')
-  //                 .get()
-  //                 .then(snapshot => {
-  //                   console.log({snapshot});//returns a query snapshot
-  //                 })
-    
-  //   console.log({posts})
-  
-//Add Post to database
-  // handleCreate = post => {
-  //    firestore.collection('post').add(post);
-   
-  // };
-
-  // handleRemove = async (id) => {
-  //   firestore.doc(`post/${id}`).delete();//removes it from database
-  // }
 
   render() {
 
--- think-piece/src/components/Posts.js
+++ think-piece/src/components/Posts.js
@@ -5,26 +5,14 @@ import { PostsContext } from '../providers/PostsProvider';
 
 
 
-// const Posts = ({ posts}) => {
-//   return (
-//     <section className="Posts">
-//       <AddPost/>
-//       {posts.map(post => <Post {...post} key={post.id}/>)}
-//     </section>
-//   )
-// }
-
-// export default Posts;
-
 const Posts = () => {
   const posts = useContext(PostsContext);
 
   return (
     <section className="Posts">
       <AddPost/>
-      {/* <PostsContext.Consumer> */}
+
           {posts.map(post => <Post {...post} key={post.id}/>)}
-      {/* </PostsContext.Consumer> */}
         
     </section>
   )
